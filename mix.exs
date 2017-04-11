4d7eaf1cd326cc260055449e1484b0635e491fd2
niku <niku@niku.name>
2017-04-11 13:10:29 +0900

    Use credo to analyse codes statically
    $ mix credo.gen.config && mv .credo.exs config/

--- mix.exs
+++ mix.exs
@@ -29,6 +29,7 @@ defmodule Hobot.Publisher.TwitterStreaming.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.14", only: [:dev, :test], runtime: false}]
+    [{:ex_doc, "~> 0.14", only: [:dev, :test], runtime: false},
+     {:credo, "~> 0.7", only: [:dev, :test], runtime: false}]
   end
 end
