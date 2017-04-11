0d6e48f0a39c06d9ced2b187d674679e2b17b965
niku <niku@niku.name>
2017-04-11 12:35:12 +0900

    Import project
    $ mix new hobot_publisher_twitter_streaming --module Hobot.Publisher.TwitterStreaming --sup

---
+++ lib/hobot_publisher_twitter_streaming.ex
@@ -0,0 +1,18 @@
+defmodule Hobot.Publisher.TwitterStreaming do
+  @moduledoc """
+  Documentation for Hobot.Publisher.TwitterStreaming.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hobot.Publisher.TwitterStreaming.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hobot_publisher_twitter_streaming_test.exs
@@ -0,0 +1,8 @@
+defmodule Hobot.Publisher.TwitterStreamingTest do
+  use ExUnit.Case
+  doctest Hobot.Publisher.TwitterStreaming
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
