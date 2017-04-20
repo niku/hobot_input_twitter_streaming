db0f02886df730afeac8aafa20fb6e37989b15e9
niku <niku@niku.name>
2017-04-20 21:01:18 +0900

    Use simple_one_for_one strategy instead of one_for_one

--- lib/hobot_in_twitter_streaming.ex
+++ lib/hobot_in_twitter_streaming.ex
@@ -3,16 +3,23 @@ defmodule Hobot.In.TwitterStreaming do
   Documentation for Hobot.In.TwitterStreaming.
   """
 
+  use GenServer
+
+  def start_link({_oauth, _param, _topic} = args, options \\ []) do
+    GenServer.start_link(__MODULE__, args, options)
+  end
+
+  def init({oauth, param, topic}) do
+    {:ok, stream} = Task.start_link(__MODULE__, :publish, [oauth, param, topic])
+    {:ok, {oauth, param, topic, stream}}
+  end
+
   @doc"""
   Publishes tweets to Hobot's topic.
   """
-  @spec publish() :: no_return
-  def publish do
-    oauth = Application.fetch_env!(:hobot_in_twitter_streaming, :oauth)
-    param =
-      Application.fetch_env!(:hobot_in_twitter_streaming, :param)
-      |> reject_blank_param
-    topic = Application.fetch_env!(:hobot_in_twitter_streaming, :topic)
+  @spec publish(keyword(String.t), keyword(String.t), keyword(String.t)) :: no_return
+  def publish(oauth, param, topic) do
+    param = reject_blank_param(param)
 
     ExTwitter.configure(:process, oauth)
     for tweet <- ExTwitter.stream_filter(param, :infinity), do: Hobot.publish(topic, tweet)
--- lib/hobot_in_twitter_streaming/application.ex
+++ lib/hobot_in_twitter_streaming/application.ex
@@ -12,12 +12,12 @@ defmodule Hobot.In.TwitterStreaming.Application do
     children = [
       # Starts a worker by calling: Hobot.In.TwitterStreaming.Worker.start_link(arg1, arg2, arg3)
       # worker(Hobot.In.TwitterStreaming.Worker, [arg1, arg2, arg3]),
-      worker(Task, [Hobot.In.TwitterStreaming, :publish, []])
+      worker(Hobot.In.TwitterStreaming, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Hobot.In.TwitterStreaming.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Hobot.In.TwitterStreaming.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
